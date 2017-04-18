ab000cab9f0079346c93527be78680173cec1f49
sleevy <chirocam@gmail.com>
2017-04-18 16:24:23 -0400

    writeFile written but not tested

--- app/src/main/java/edu/georgiasouthern/cr04956/architecturelab9/TextActivity.java
+++ app/src/main/java/edu/georgiasouthern/cr04956/architecturelab9/TextActivity.java
@@ -3,8 +3,13 @@ package edu.georgiasouthern.cr04956.architecturelab9;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
 
 public class TextActivity extends AppCompatActivity {
     private static final String QUOTE_TEXT =
@@ -23,18 +28,44 @@ public class TextActivity extends AppCompatActivity {
             "Trees sprout up just about everywhere in computer science.";
 
     private File quoteFile;
-
+    private static final String QUOTE_DIRECTORY = "Quotes";
+    private static final String QUOTE_FILE_NAME = "knuthQuotes.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text);
 
-        quoteFile = new File(Environment.getExternalStorageDirectory(), "Quote");
+        File quoteDirectory = new File(Environment.getExternalStorageDirectory(), QUOTE_DIRECTORY);
+        if(!quoteDirectory.exists()) {
+            if(quoteDirectory.mkdirs()) {
+                Toast.makeText(this, "Directory Created", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Directory Creation Failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        quoteFile = new File(quoteDirectory, QUOTE_FILE_NAME);
+
     }
 
     public void writeFile() {
+        if(quoteFileExists()) {
+            Toast.makeText(this, "File already exists.", Toast.LENGTH_SHORT).show();
+            return;
+        }
         //TODO: implement writing
+        try {
+            FileWriter writer = new FileWriter(quoteFile);
+            writer.append(QUOTE_TEXT);
+            writer.flush();
+            writer.close();
+            Toast.makeText(this, "Quote file written successfully", Toast.LENGTH_SHORT).show();
+        } catch(IOException ioe) {
+            ioe.printStackTrace();
+            Toast.makeText(this, "Error writing quote file", Toast.LENGTH_SHORT).show();
+        }
+
 
     }
 
